24824a92d8bd94e16457cb22afb89b8359d6fdf7
Caue Campos <cauewc05@gmail.com>
2024-11-27 15:32:33 -0300

    1.2

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-int arrayNums1[2500];
-int arrayNums2[2500];
-int arrayNums3[2500];
-int arrayNums4[2500];
+int arrayNums1[100000];
+int arrayNums2[100000];
+int arrayNums3[100000];
+int arrayNums4[100000];
 double timeInsertion;
 double timeQuick;
 double timeBubble;
@@ -26,17 +28,48 @@ char palavras[100][20] = {
 };
 
 
-// função para criar um vetor com 2500 numeros aleatorios.
+// função para criar um vetor com 50000 numeros aleatorios.
 void criarNum(){
-    for(int i=0; i<2500; i++){
-    arrayNums1[i],arrayNums2[i],arrayNums3[i],arrayNums4[i]= rand() % 2500;
+    for(int i = 0; i < 100000; i++){
+        int num = rand() % 100000;
+        arrayNums1[i] = num;
+        arrayNums2[i] = num;
+        arrayNums3[i] = num;
+        arrayNums4[i] = num;
     }
 }
 
 // METODOS DE ORDENAÇÃO
+void insertionSort(int vet[], int tam) {
 
+    clock_t start = clock();
 
+    int i, j, aux;
+        for (i = 1; i < tam; i++){
+            aux = vet[i];
+            j = i - 1;
+            
+            while((j >= 0) && (aux < vet[j])){
+                vet[j+1] = vet[j];
+                j--;
+            }
+            vet[j+1]= aux;
+        }
 
+    clock_t end = clock();
+    timeInsertion = (double)(end - start) / CLOCKS_PER_SEC;
+
+}
+
+// MOSTRAR OS TEMPOS
+void showTimes(double x){//,double y,double z, double a) {
+        printf("------- TEMPOS DE EXECUCAO -------\n");
+        printf("Insertion Sort: %f segundos\n", x);
+//      printf("Selection Sort: %f segundos\n", y);
+//      printf("Bubble Sort: %f segundos\n", z);
+//      printf("Quick Sort: %f segundos\n", a);
+        printf("----------------------------------\n");
+}
 
 
 
@@ -49,11 +82,12 @@ int main () {
     scanf("%d", &resp);
 
     if(resp==1){
-        printf("Criando uma ordem de numeros aleatorios...\n");
-        criarNum();
-        printf("Ordenando os numeros...\n");
+            printf("Criando uma ordem de numeros aleatorios...\n");
+            criarNum();
+            printf("Ordenando os numeros...\n\n");
+            insertionSort(arrayNums1,100000);
+            showTimes(timeInsertion);
 
-        //totalTime();
     } else if(resp==2) {
         printf("Criando uma ordem de nomes aleatorios...\n");
         printf("Ordenando os nomes...\n");
